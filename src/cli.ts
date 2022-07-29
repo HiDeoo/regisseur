8d65478bfc45ca524e6f0ad6b8d471cda8cd5808
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-07-29 10:06:46 +0200

    feat: add CLI basic structure

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,28 @@
-function run() {
-  console.log('plop2')
+import cac from 'cac'
+
+const cli = cac('regisseur')
+
+cli.help()
+
+cli.command('[file]', '// TODO').action(() => {
+  console.log('no command')
+})
+
+cli.command('validate [file]', '// TODO').action(() => {
+  console.log('validate')
+})
+
+async function run() {
+  try {
+    cli.parse(process.argv, { run: false })
+
+    await cli.runMatchedCommand()
+  } catch (error) {
+    // TODO(HiDeoo)
+    console.log('🚨 [cli.ts:12] error', error)
+
+    process.exit(1)
+  }
 }
 
 run()
-
-export {}

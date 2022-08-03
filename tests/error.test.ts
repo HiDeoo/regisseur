f1699d89d71ca612dbaf6a82c208f81011b8c58d
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-08-03 11:43:07 +0200

    test: validation error

--- tests/error.test.ts
+++ tests/error.test.ts
@@ -1,8 +1,9 @@
-import { expect, test } from 'vitest'
+import { expect, test, vi } from 'vitest'
+import { z, ZodError } from 'zod'
 
-import { UserAbortError } from '../src/libs/error'
+import { logValidationError, UserAbortError } from '../src/libs/error'
 
-test('should log the command to resume play', async () => {
+test('should log the command to resume a play', async () => {
   expect(
     new UserAbortError(
       {
@@ -17,4 +18,35 @@ test('should log the command to resume play', async () => {
     "
     To resume from act #5, use 'regisseur test.play -c 5'."
   `)
+})
+
+test('should log a validation error', async () => {
+  expect.assertions(2)
+
+  try {
+    const schema = z.object({
+      title: z.string(),
+      count: z.number(),
+    })
+
+    schema.parse({ title: 1, count: 'a' })
+  } catch (error) {
+    const consoleErrorSpy = vi.spyOn(console, 'error').mockImplementation(() => undefined)
+
+    if (error instanceof ZodError) {
+      logValidationError(error)
+    }
+
+    expect(consoleErrorSpy).toHaveBeenCalledOnce()
+    expect(consoleErrorSpy.mock.lastCall).toMatchInlineSnapshot(`
+      [
+        "Validation error:",
+        "
+      	Code: invalid_type - Path: title - Message: Expected string, received number
+      	Code: invalid_type - Path: count - Message: Expected number, received string",
+      ]
+    `)
+
+    consoleErrorSpy.mockRestore()
+  }
 })

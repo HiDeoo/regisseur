c5be13dbb742e7b7434aba199b9e79e8b3b81f2e
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-08-03 11:44:01 +0200

    refactor: pluralize

--- src/libs/string.ts
+++ src/libs/string.ts
@@ -1,9 +1,3 @@
-export function pluralize(count: number, word: string): string
-export function pluralize(count: number, singular: string, plural: string): string
-export function pluralize(count: number, wordOrSingular: string, plural?: string): string {
-  if (!plural) {
-    return wordOrSingular + (count === 1 ? '' : 's')
-  }
-
-  return count === 1 ? wordOrSingular : plural
+export function pluralize(count: number, word: string): string {
+  return word + (count === 1 ? '' : 's')
 }
